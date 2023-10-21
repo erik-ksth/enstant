0d881c0f2c561bb4fbfe3bedeceb9eb46ded4dc0
Erik Hein <erikhein.ksth@gmail.com>
2023-10-20 17:39:38 -0700

    feat: front-end desktop header

--- src/components/elements/Button/index.js
+++ src/components/elements/Button/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 import '../../../App.css'
 import '../../../index.css';
 
 const NavBtn = ({ label, onClick }) => {
   return (
-    <button className="navBtn" onClick={onClick}>
-      {label}
-    </button>
+    <Button variant='contained' sx={
+      {backgroundColor: `var(--primary-blue)`,
+        textTransform: 'none',
+        fontFamily: `NatsReg`,
+        fontSize: '20px',
+        padding: '0px 25px',
+      }
+    }>{label}</Button>
   );
 };
 
--- src/components/modules/PageLayout/submodules/AppHeader/index.js
+++ src/components/modules/PageLayout/submodules/AppHeader/index.js
@@ -1,26 +1,63 @@
 import * as React from 'react';
 // import AppBar from '@mui/material/AppBar';
-// import Box from '@mui/material/Box';
+import Box from '@mui/material/Box';
 // import Toolbar from '@mui/material/Toolbar';
 // import IconButton from '@mui/material/IconButton';
 // import Typography from '@mui/material/Typography';
 // import Menu from '@mui/material/Menu';
 // import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-// import Tabs from '@mui/material/Tabs';
-// import Button from '@mui/material/Button';
 // import Tooltip from '@mui/material/Tooltip';
 // import MenuItem from '@mui/material/MenuItem';
-import '../../../../../index.css'
+import { Link } from '@mui/material';
+import '../../../../../index.css';
+import NavBtn from '../../../../elements/Button';
 
 // const pages = ['Home', 'Demo', 'FAQs', 'About Us', 'Contact Us'];
 
 function NavBar() {
 
   return (
-    <Container>
-
+    <Container sx={navBarContainer}>
+      <Box sx={logoContainer}>
+          <Link sx={{ color: `var(--primary-blue)`, textDecoration: 'none', fontWeight: '600'}} href="/">enstant.com</Link>
+      </Box>
+      <Box sx={navBtnContainer}>
+          <Link sx={navBtn} href="/">Home</Link>
+          <Link sx={navBtn} href="/">Demo</Link>
+          <Link sx={navBtn} href="/">About Us</Link>
+          <Link sx={navBtn} href="/">Contact Us</Link>
+      </Box>
+      <Box sx={getStartedBtnContainer}>
+        <NavBtn label='Get Started'/>
+      </Box>
     </Container>
   );
 }
 export default NavBar;
+
+const navBarContainer = {
+  display: 'flex',
+  justifyContent: 'space-between'
+}
+
+const logoContainer = {
+  width: "15%",
+}
+
+const navBtnContainer = {
+  width: "70%",
+  display: 'flex',
+  justifyContent: 'center'
+}
+
+const getStartedBtnContainer = {
+  width: "15%",
+  textAlign: 'right',
+}
+
+const navBtn = {
+  textDecoration: 'none',
+  color: `var(--inactive-text-color)`,
+  margin: '0 4%'
+}
