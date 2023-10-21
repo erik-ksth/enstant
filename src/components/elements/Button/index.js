afe182e33fcfc8a342306d6911f2523f7d961235
Erik Hein <erikhein.ksth@gmail.com>
2023-10-20 19:34:02 -0700

    feat: front-end desktop heroSection

--- src/components/elements/Button/index.js
+++ src/components/elements/Button/index.js
@@ -3,17 +3,17 @@ import Button from '@mui/material/Button';
 import '../../../App.css'
 import '../../../index.css';
 
-const NavBtn = ({ label, onClick }) => {
+const Btn = ({ label, onClick }) => {
   return (
     <Button variant='contained' sx={
       {backgroundColor: `var(--primary-blue)`,
         textTransform: 'none',
         fontFamily: `NatsReg`,
         fontSize: '20px',
-        padding: '0px 25px',
+        padding: '5px 25px',
       }
     }>{label}</Button>
   );
 };
 
-export default NavBtn;
+export default Btn;
