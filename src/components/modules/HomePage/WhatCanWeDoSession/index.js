d26d8c530de2fac76abd2f4981dadaba28520e17
Erik Hein <erikhein.ksth@gmail.com>
2023-10-24 16:21:12 -0700

    feat: what can we do for you session

---
+++ src/components/modules/HomePage/WhatCanWeDoSession/index.js
@@ -0,0 +1,75 @@
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import React from "react";
+
+function WhatCanWeDo() {
+    return (
+        <div style={parentContainer}>
+            <h2 className="contrastText">What <span className="contrastBold">can we do</span> for you?</h2>
+
+            <ul style={listContainer}>
+
+                <li style={list}>
+                    <CheckCircleOutlineIcon style={checkedIcon}/>
+                    <p className='contrastText'>enstant.com provides you with a
+                        <span className='contrastBold'> quicker, easier and smarter solution </span>
+                        to manage your Corporate or Organization business card orders.
+                    </p>
+                </li>
+
+                <li style={list}>
+                    <CheckCircleOutlineIcon style={checkedIcon}/>
+                    <p className='contrastText'>enstant.com allows you to
+                        <span className='contrastBold'> place </span> orders,
+                        <span className='contrastBold'> poof </span>
+                        for typos and view up-to-the-minute order
+                        <span className='contrastBold'> status </span> and 
+                        <span className='contrastBold'> history </span>
+                        any time from anywhere using internet!
+                    </p>
+                </li>
+
+                <li style={list}>
+                    <CheckCircleOutlineIcon style={checkedIcon}/>
+                    <p className='contrastText'>enstant.com sets up a
+                        <span className='contrastBold'> password protected private website </span>
+                        for your business cards, ensuring
+                        <span className='contrastBold'> consistency </span>
+                        in logo, fonts, layout, and link colors, etc.
+                    </p>
+                </li>
+
+                <li style={list}>
+                    <CheckCircleOutlineIcon style={checkedIcon}/>
+                    <p className='contrastText'>Users can
+                        <span className='contrastBold'> instantly preview </span>
+                        an online proof, speeding up the proofing cycle and preventing errors.
+                    </p>
+                </li>
+
+            </ul>
+        </div>
+    )
+}
+export default WhatCanWeDo
+
+const parentContainer = {
+    width: '100%',
+    backgroundColor: `var(--text-color)`,
+    padding: '3% 0'
+}
+
+const checkedIcon = {
+    color: 'white',
+    fontSize: '2em',
+}
+
+const listContainer = {
+    padding: ' 02%10%'
+}
+
+const list = {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    columnGap: '2%'
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,15 @@ import Container from '@mui/material/Container';
 import Navbar from '../components/modules/PageLayout/submodules/AppHeader/index'
 import HeroSection from '../components/modules/HomePage/HeroSection'
 import WhatDoYouWant from '../components/modules/HomePage/WhatDoYouWantSection'
+import WhatCanWeDo from '../components/modules/HomePage/WhatCanWeDoSession'
 
 function Home() {
   return (
      <body>
-          <Container maxWidth="2000px">
-               <Navbar/>
-               <HeroSection/>
-               <WhatDoYouWant/>
-          </Container>
+          <Navbar/>
+          <HeroSection/>
+          <WhatDoYouWant/>
+          <WhatCanWeDo/>
      </body>
   );
 }
