c8ff3168ea71dfd7873e4ce5259033a2c9ab6dd0
Erik Hein <erikhein.ksth@gmail.com>
2023-11-06 16:12:32 -0800

    feat: navbar color change

--- src/components/modules/PageLayout/submodules/AppHeader/index.js
+++ src/components/modules/PageLayout/submodules/AppHeader/index.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import {React, useState} from "react";
 import { NavLink, Link, useLocation } from "react-router-dom";
 import "../../../../../index.css";
 import Btn from "../../../../elements/Button";
@@ -8,36 +8,49 @@ import "./Header.css";
 
 function NavBar() {
   const location = useLocation();
+  const [colorChange, setColorChange] = useState(false);
+  const changeNavBarColor = () => {
+    if (window.scrollY >= 10) {
+      setColorChange(true);
+    } else {
+      setColorChange(false);
+    }
+  };
+  window.addEventListener("scroll", changeNavBarColor);
   return (
-    <div className="container">
-      <div className="logoContainer">
-        <Link className="logo" to="/">
-          enstant.com
-        </Link>
-      </div>
-      {/* <div className="navBarContainer">
-        <Link className="navBtn" to="/">Home</Link>
-        <Link className="navBtn" to="/demo">Demo</Link>
-        <Link className="navBtn" to="/about">About Us</Link>
-        <Link className="navBtn" to="/contactus">Contact Us</Link>
-      </div> */}
-      
-      <div className="navBarContainer">
-      <NavLink exact to="/" className={`navBtn ${location.pathname === "/" ? "active-tab" : ""}`}>
-          Home
-        </NavLink>
-        <NavLink to="/demo" className={`navBtn ${location.pathname === "/demo" || location.pathname === "/startDemo" ? "active-tab" : ""}`}>
-          Demo
-        </NavLink>
-        <NavLink to="/about" className={`navBtn ${location.pathname === "/about" ? "active-tab" : ""}`}>
-          About Us
-        </NavLink>
-        <NavLink to="/contactUs" className={`navBtn ${location.pathname === "/contactUs" ? "active-tab" : ""}`}>
-          Contact Us
-        </NavLink>
-      </div>
-      <div className="getStartedBtnContainer">
-        <Btn label="Get Started" to="/getStarted"/>
+    <div className={
+      colorChange ? "mainContainer colorChange" : "mainContainer"
+    }>
+      <div className="container">
+        <div className="logoContainer">
+          <Link className="logo" to="/">
+            enstant.com
+          </Link>
+        </div>
+        {/* <div className="navBarContainer">
+          <Link className="navBtn" to="/">Home</Link>
+          <Link className="navBtn" to="/demo">Demo</Link>
+          <Link className="navBtn" to="/about">About Us</Link>
+          <Link className="navBtn" to="/contactus">Contact Us</Link>
+        </div> */}
+        
+        <div className="navBarContainer">
+        <NavLink exact to="/" className={`navBtn ${location.pathname === "/" ? "active-tab" : ""}`}>
+            Home
+          </NavLink>
+          <NavLink to="/demo" className={`navBtn ${location.pathname === "/demo" || location.pathname === "/startDemo" ? "active-tab" : ""}`}>
+            Demo
+          </NavLink>
+          <NavLink to="/about" className={`navBtn ${location.pathname === "/about" ? "active-tab" : ""}`}>
+            About Us
+          </NavLink>
+          <NavLink to="/contactUs" className={`navBtn ${location.pathname === "/contactUs" ? "active-tab" : ""}`}>
+            Contact Us
+          </NavLink>
+        </div>
+        <div className="getStartedBtnContainer">
+          <Btn label="Get Started" to="/getStarted"/>
+        </div>
       </div>
     </div>
   );
